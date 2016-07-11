38632e99f8cd40a05ddc1beec4142f819b9fd8ea
Isaac Yoon <realisy@gmail.com>
2016-07-11 00:32:14 -0400

    done except enhancements

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,24 @@ get '/messages' do
 end
 
 get '/messages/new' do
+  @message = Message.new
   erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title:   params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
 end
